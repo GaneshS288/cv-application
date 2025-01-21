f4dcefa5676fa42c4f3c0fead1a9624b166d2990
Ganesh <115623665+GaneshS288@users.noreply.github.com>
2025-01-21 14:07:58 +0530

    create education form inputs

---
+++ src/components/EducationInputList.jsx
@@ -0,0 +1,37 @@
+export default function EducationInputList({educationInfo}) {
+  return (
+    <fieldset>
+      <legend>Education</legend>
+      {
+        educationInfo.map((info) => {
+            return (<EducationInputItem key={info.id} institute={info.institute} certificateTitle={info.title} year={info.year} gpa={info.gpa}></EducationInputItem>);
+        })
+      }
+    </fieldset>
+  );
+}
+
+function EducationInputItem({institute, certificateTitle, year, gpa}) {
+  return (
+    <div className="education-input-item flex-column">
+      <label>
+        Intstitue :
+        <input type="text" name="institute" value={institute} placeholder="Institute Name" required />
+      </label>
+
+      <label>
+        Certificate Title :
+        <input type="text" name="certificate-title" value={certificateTitle} placeholder="B.Sc. (Honours)" required />
+      </label>
+
+      <label>
+        Year :
+        <input type="number" name="certification-year" value={year} placeholder="2020" required />
+      </label>
+
+      <label>
+        <input type="text" name="gpa" value={gpa} placeholder="4.2"/>
+      </label>
+    </div>
+  );
+}
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,31 @@
-import "../styles/Sidebar.css"
-import GeneralInfoInputs from "./General-Info-inputs"
+import "../styles/Sidebar.css";
+import EducationInputList from "./EducationInputList";
+import GeneralInfoInputs from "./General-Info-inputs";
+
+const educationInfo = [
+  {
+    id: crypto.randomUUID(),
+    institute: "Abc university",
+    title: "B.Sc. (Honours)",
+    year: 2020,
+    gpa: 4.2,
+  },
+  {
+    id: crypto.randomUUID(),
+    institute: "Abc university",
+    title: "B.Sc. (Honours)",
+    year: 2020,
+    gpa: 4.2,
+  },
+];
 
 export default function Sidebar() {
-    return(
-        <div className="sidebar">
-            <GeneralInfoInputs></GeneralInfoInputs>
-        </div>
-    )
+  return (
+    <div className="sidebar">
+      <form action="" method="dialog">
+        <GeneralInfoInputs></GeneralInfoInputs>
+        <EducationInputList educationInfo={educationInfo}></EducationInputList>
+      </form>
+    </div>
+  );
 }
